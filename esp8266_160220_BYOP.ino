c7c0f1d399d54e8b5363a5131cc0014e0db3269e
Yasuhiko OKADA <yasokada@gmail.com>
2016-02-20 08:16:09 +0900

    add Serial setup and hello printout

--- esp8266_160220_BYOP.ino
+++ esp8266_160220_BYOP.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
 
+/*
+ * v0.1 2016 Feb 20
+ *  - 
+ */
+
+void setup() {
+  Serial.begin(115200); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Hello");
+  delay(2000);
 }
