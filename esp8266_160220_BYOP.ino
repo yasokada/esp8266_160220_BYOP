afee95caf81d1af601e4619ab6cbcabfeae1b3bc
Yasuhiko OKADA <yasokada@gmail.com>
2016-02-20 10:22:52 +0900

    add extractCsvRow() / Test_extractCsvRow()

--- esp8266_160220_BYOP.ino
+++ esp8266_160220_BYOP.ino
@@ -6,6 +6,8 @@
 
 
 /*
+ * v0.4 2016 Feb 20
+ *  - add extractCsvRow() / Test_extractCsvRow()
  * v0.3 2016 Feb 20
  *  - move static declaraions for UART to UARTlib
  *  - move Serial_readCommand to UARTlib
@@ -18,16 +20,59 @@
  *  - add serial setup
  */
 
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+
 void setup() {
   Serial.begin(115200); 
 }
 
-void Serial_replyToCommand(String cmdline)
+void Test_extractCsvRow()
 {
+  String csvline = "AAA,BBB,CCC";
+  String tmp;
+  for(int idx = 0; idx < 3; idx++) {
+    tmp = extractCsvRow(csvline, idx);
+    Serial.println(tmp);    
+  }
+}
+
+String extractCsvRow(String srcline, int getIdx)
+{
+    int commaPos = -1; // to start from 0 by (commadPos + 1), this requires -1 here
+    int posIdx = 0;
+    int nextOfComma;
+
+    while(1) {
+      nextOfComma = commaPos + 1;
+      commaPos = srcline.indexOf(',', nextOfComma);
+      if (commaPos < 0) {
+        break;
+      }
+      if (posIdx == getIdx) {
+        break; 
+      }
+      posIdx++;
+    }
+    commaPos = srcline.indexOf(',', nextOfComma);
+    if (commaPos < 0) {
+      return srcline.substring(nextOfComma);
+    } else {
+      return srcline.substring(nextOfComma, commaPos);
+    }
+}
+
+void Serial_replyToCommand(String cmdline)
+{  
   if (cmdline.equalsIgnoreCase("hello")) {
     Serial.println("hello, 7of9");
   } else {
+#if 1
+    Test_extractCsvRow();
+#else      
     Serial.println(cmdline);    
+#endif    
   }
 }
 
