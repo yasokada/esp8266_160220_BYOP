f5082e63b998bd088ec466541874ea3c55def74a
Yasuhiko OKADA <yasokada@gmail.com>
2016-02-22 07:11:17 +0900

    add s_messageList[], [s_messageCount]

--- esp8266_160221_msgStorageLib.ino
+++ esp8266_160221_msgStorageLib.ino
@@ -1,11 +1,22 @@
 
 // String library
 
+#include "dataStructure.h"
+
 /*
+ * v0.2 2016 Feb. 22
+ *  - add s_messageList[], [s_messageCount]
+ *  - add Test_MsgServer_setupDummyMessages()
  * v0.1 2016 Feb. 21
  *  - prepare empty functions
  */
 
+//-------------------------------------------------------------------------
+// file scope static functions / variables
+
+static const int kMaxnum_MessageCount = 10; // TODO: 0m > check memory size
+static message_t s_messageList[kMaxnum_MessageCount];
+static int s_messageCount = 0;
 //-------------------------------------------------------------------------
 
 void MsgServer_Clear()
@@ -34,6 +45,11 @@ void MsgServer_PostMessage(String snder, String rcver, String msg)
 }
 
 void MsgServer_RemoveMessage(int idx)
+{
+
+}
+
+void Test_MsgServer_setupDummyMessages()
 {
 
 }
