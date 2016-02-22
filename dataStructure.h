b85d6a7ea44172de7a8865123089edd0cbda4bbf
Yasuhiko OKADA <yasokada@gmail.com>
2016-02-23 07:13:22 +0900

    msgStorageLib: impl Test_MsgServer_postThenGet()

--- dataStructure.h
+++ dataStructure.h
@@ -7,17 +7,6 @@ typedef struct tag_pizero_owner_t {
   String nickName;
 } Pizero_owner_t;
 
-#if 0
-// Message structure
-typedef struct tag_message_t {
-  String senderSerial; // serial number of the sender
-  String senderName; // name of the sender
-  String receiverName; // name of the receiver
-  String message;
-  bool isSecret; // sercrete message or normal message
-} message_t;
-#endif
-
 String kCmdList[] = {
   "hello",
   "check",
--- esp8266_160221_msgStorageLib.ino
+++ esp8266_160221_msgStorageLib.ino
@@ -1,10 +1,13 @@
 
 // String library
 
-//#include "dataStructure.h"
 #include "msgStorage.h"
 
 /*
+ * v0.5 2016 Feb. 23
+ *  - impl Test_MsgServer_postThenGet()
+ * v0.4 2016 Feb. 23
+ *  - use [msgStorage.h]
  * v0.3 2016 Feb. 23
  *  - add Test_MsgServer_postThenGet()
  *  - add MsgServer_PostMessage()
@@ -29,8 +32,10 @@ static int s_messageCount = 0;
 
 static const message_t s_dummyMsg[] =
 {
-	{ "001", "7of9", "Vital", "hello,Vital", 0 },
-	{ "007", "Vital", "7of9", "hello,7of9", 0 },
+	{ "000000000000000d", "7of9", "Vital", "hello,Vital", 0 },
+	{ "000000000000002g", "Vital", "7of9", "hello,7of9", 0 },
+	{ "000000000000000d", "7of9", "Vital", "meet at the Arctic", 1 },
+	{ "000000000000002g", "Vital", "7of9", "No,thank you.", 1 },
 };
 //-------------------------------------------------------------------------
 
@@ -54,7 +59,7 @@ void MsgServer_Remove1stMessage(String rcver)
 	// TODO: 0m > remove 1st message for the receiver
 }
 
-bool MsgServer_PostMessage(String snder, String rcver, String msg, bool isSecret)
+bool MsgServer_PostMessage(String srl, String snder, String rcver, String msg, bool isSecret)
 {
 	// TODO: 0m > add message 
 }
@@ -65,9 +70,9 @@ bool MsgServer_PostMessage(String snder, String rcver, String msg, bool isSecret
 
 void Test_MsgServer_setupDummyMessages()
 {
-	int size = sizeof(s_dummyMsg) / sizeof(s_dummyMsg[0]);
+	int dmysiz = sizeof(s_dummyMsg) / sizeof(s_dummyMsg[0]);
 
-	for(int idx = 0; idx < size; idx++) {
+	for(int idx = 0; idx < dmysiz; idx++) {
 		s_messageList[idx].senderSerial = s_dummyMsg[idx].senderSerial;
 		s_messageList[idx].senderName 	= s_dummyMsg[idx].senderName;
 		s_messageList[idx].receiverName = s_dummyMsg[idx].receiverName;
@@ -76,7 +81,7 @@ void Test_MsgServer_setupDummyMessages()
 	}
 
 	String work;
-	for(int idx = 0; idx < size; idx++) {
+	for(int idx = 0; idx < dmysiz; idx++) {
 		work = s_messageList[idx].senderSerial
 			+ "," + s_messageList[idx].senderName
 			+ "," + s_messageList[idx].receiverName
@@ -90,4 +95,36 @@ void Test_MsgServer_setupDummyMessages()
 void Test_MsgServer_postThenGet()
 {
 
+	int dmysiz = sizeof(s_dummyMsg) / sizeof(s_dummyMsg[0]);
+
+// bool MsgServer_PostMessage(String srl, String snder, String rcver, String msg, bool isSecret)
+
+	String srl; // seraial of the sender
+	String snder; // sender
+	String rcver; // receiver
+	String msg; // message
+	bool isScr; // is secret message?
+
+	// 1. post
+	for(int idx = 0; idx < dmysiz; idx++) {
+		srl = s_dummyMsg[idx].senderSerial;
+		snder = s_dummyMsg[idx].senderName;
+		rcver = s_dummyMsg[idx].receiverName;
+		msg = s_dummyMsg[idx].message;
+		isScr = s_dummyMsg[idx].isSecret;
+		MsgServer_PostMessage(srl, snder, rcver, msg, isScr);
+	}
+
+	// 2. get
+	String iam = "7of9";
+	int msgCnt = MsgServer_GetMessageCount(iam);
+
+	String msgToMe;
+	for(int idx = 0; idx < msgCnt; idx++) {
+		msgToMe = MsgServer_Get1stMessage(iam);
+		debug_outputDebugString("Test_MsgServer_postThenGet", "Line125 > " + msgToMe);		
+		MsgServer_Remove1stMessage(iam);
+	}
+
+	// TODO: 0m > use MsgServer_Clear();
 }
