ff04d5f88ccca548568f2882429a362525043250
Yasuhiko OKADA <yasokada@gmail.com>
2016-02-27 09:05:52 +0900

    filesysLib: add read/write uint8_t()

--- esp8266_160227_filesysLib.ino
+++ esp8266_160227_filesysLib.ino
@@ -13,8 +13,11 @@ Checked on staging 2.1.0-rc1 Arduino IDE 1.6.6 (2016 Feb. 27)
 */
 
 /*
- * v0.3 2016 Feb. 27
- *  - add FileSys_write_uint8_t()
+ * v0.4 2016 Feb. 27
+ *  - add read/write uint8_t()
+ *		+ add Test_read_write_uint8_t()
+ *  	+ add FileSys_read_uint8_t()
+ *  	+ add FileSys_write_uint8_t()
  * v0.2 2016 Feb. 27
  *  - add File_init()
  * v0.1 2016 Feb. 27
@@ -35,5 +38,22 @@ void FileSys_write_uint8_t(uint8_t addr, uint8_t srcval)
 	EEPROM.commit();
 }
 
+uint8_t FileSys_read_uint8_t(uint8_t addr)
+{
+	uint8_t retval = EEPROM.read(addr);
+	return retval;	
+}
+
+void Test_read_write_uint8_t()
+{
+	uint8_t wrval = 79;
+	uint8_t addr = 0;
+	uint8_t rdval;
+
+	FileSys_write_uint8_t(addr, wrval);
+	rdval = FileSys_read_uint8_t(addr);
+
+	debug_outputDebugString("Test_read_write_uint8_t","Line55 > " + String(rdval));
+}
 
 
